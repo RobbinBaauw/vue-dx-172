5ce0e1579a72c6e4e09392c8a97bec648401586c
Robbin Baauw <robbin.baauw@gmail.com>
2021-01-21 09:45:48 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <input type="number" v-model="myNumber" />
+  <input type="checkbox" v-model="myCheckbox" />
+</template>
+
+<script lang="ts">
+import { ref, watchEffect, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup: () => {
+    const myNumber = ref(0);
+    watchEffect(() => console.log(myNumber.value));
+
+    const myCheckbox = ref(false);
+    watchEffect(() => console.log(myCheckbox.value));
+
+    return { myNumber, myCheckbox }
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
